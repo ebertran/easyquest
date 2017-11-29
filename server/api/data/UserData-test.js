b8897b73e6f1b4441be6a0888ca4f0b8d5ec879b
manuelbarzi <manuelbarzi@gmail.com>
2017-11-29 13:35:00 +0100

    correct user data add quiz

--- server/api/data/UserData-test.js
+++ server/api/data/UserData-test.js
@@ -6,34 +6,36 @@ const userData = new (require("./UserData"))();
 
 const assert = require("assert");
 
-// userData
-//   .list()
-//   .then(assertList)
-//   .catch(assertList);
-
-// function assertList(res) {
-//   assert.ok(!(res instanceof Error), `should get results without error ${res}`);
-
-//   assert.ok(res, "results should not be empty");
-
-//   assert.ok(res.length > 0, "results should have values");
-
-//   console.log("userData.list OK");
-// }
+// list
 
 userData
-  .addQuiz("5a04c5ae1d195c4e88dbfcab", "5a1e9c8ebf5dd9069fb29b31", {
-    questions: [
-      {
-        _id: "5a1e9c8ebf5dd9069fb29b35",
-        answer: "5a1e9c8ebf5dd9069fb29b36"
-      },
-      {
-        _id: "5a1e9c8ebf5dd9069fb29b32",
-        answer: "5a1e9c8ebf5dd9069fb29b33"
-      }
-    ]
-  })
+  .list()
+  .then(assertList)
+  .catch(assertList);
+
+function assertList(res) {
+  assert.ok(!(res instanceof Error), `should get results without error ${res}`);
+
+  assert.ok(res, "results should not be empty");
+
+  assert.ok(res.length > 0, "results should have values");
+
+  console.log("userData.list OK");
+}
+
+// add quiz
+
+userData
+  .addQuiz("5a04c5ae1d195c4e88dbfcaa", "5a1e9c8ebf5dd9069fb29b31", [
+    {
+      _id: "5a1e9c8ebf5dd9069fb29b35",
+      answer: "5a1e9c8ebf5dd9069fb29b36"
+    },
+    {
+      _id: "5a1e9c8ebf5dd9069fb29b32",
+      answer: "5a1e9c8ebf5dd9069fb29b33"
+    }
+  ])
   .then(assertAddQuiz)
   .catch(assertAddQuiz);
 
@@ -42,7 +44,7 @@ function assertAddQuiz(res) {
 
   assert.ok(res, "results should not be empty");
 
-  assert.ok(res.length > 0, "results should have values");
+  assert.ok(res._id, "results should have _id");
 
   console.log("userData.addQuiz OK");
 }
--- server/api/data/UserData.js
+++ server/api/data/UserData.js
@@ -81,21 +81,24 @@ class UserData {
     });
   }
 
-  addQuiz(userId, quizId, answers) {
+  addQuiz(userId, quizId, questions) {
     return new Promise((resolve, reject) => {
       if (!userId) throw new Error(`userId cannot be ${userId}`);
       if (!quizId) throw new Error(`quizId cannot be ${quizId}`);
-      if (!answers) throw new Error(`answers cannot be ${answers}`);
+      if (!questions) throw new Error(`answers cannot be ${questions}`);
 
       User.findByIdAndUpdate(userId, {
         $push: { quizs:
           {
-            "_id": quizId,
-            "questions": answers
+            _id: quizId,
+            questions
           }
       }})
+        .then(() => {
+          return this.retrieve(userId)
+        })
         .then(resolve)
-        .catch(reject);
+        .catch(reject)
     });
   }
 }
